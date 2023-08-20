f03f75083023fe51ed7fc0c450d3780acc4b6d91
EmrikeDev <142704362+EmrikeDev@users.noreply.github.com>
2023-08-20 14:18:31 +0800

    Add files via upload

---
+++ xcteam/members/script.js
@@ -0,0 +1,34 @@
+const personContainer = document.getElementById('personContainer');
+
+data.sort((a, b) => parseInt(a.id) - parseInt(b.id));
+
+data.forEach(person => {
+	const personCard = document.createElement('div');
+	personCard.className = 'person-card';
+
+	const personImageContainer = document.createElement('div');
+	personImageContainer.className = 'person-image';
+	const personImage = document.createElement('img');
+	personImage.src = `faces/${person.id}.jpg`;
+	personImage.alt = person.name;
+	personImageContainer.appendChild(personImage);
+
+	const personName = document.createElement('div');
+	personName.className = 'person-name';
+	personName.textContent = person.name;
+
+	const personInfo = document.createElement('div');
+	personInfo.className = 'person-info';
+	personInfo.textContent = person.info;
+
+	personCard.appendChild(personImageContainer);
+	personCard.appendChild(personName);
+	personCard.appendChild(personInfo);
+
+	personCard.addEventListener('click', () => {
+		const uid = person.uid;
+		window.location.href = `https://space.bilibili.com/${uid}`;
+	});
+
+	personContainer.appendChild(personCard);
+});
